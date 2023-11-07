6077bd7605d5bc529fdaff869a5e2e5f6cc36b85
tony <tony@miettas.com.au>
2023-11-07 16:22:25 +1100

    viteerror

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
-    @vite(['resources/css/app.css', 'public/css/app.css', 'public/css/knox_screen.css', 'resources/js/app.js', 'public/js/app.js', 'public/js/picturefill.min.js'])
+    @vite(['resources/css/app.css', 'public/css/knox_screen.css', 'resources/js/app.js', 'public/js/app.js', 'public/js/picturefill.min.js'])
     
 </head>
 <body>
