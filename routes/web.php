24d49db403a0bea3977b893d7e4dfd0282fc129d
tony <tony@miettas.com.au>
2023-11-07 16:42:31 +1100

    directory error

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/', [App\Http\Controllers\DirectoriesController::class, 'welcome'])->name('welcome');
 
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
